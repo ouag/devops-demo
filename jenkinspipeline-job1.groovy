2acba0a0c073c380b8a5c63e388703368c1e3228
mouag <mo.ouag@gmail.com>
2021-11-14 22:55:19 +0100

    adding pipeline

--- jenkinspipeline-job1.groovy
+++ jenkinspipeline-job1.groovy
@@ -1,5 +1,7 @@
 @Library('folder_shared_lib') _
 
+Log2 log2 = new Log2()
+
 pipeline {
     agent any
 
@@ -8,7 +10,7 @@ pipeline {
             steps {
 
                 script {
-                    Log2 log2 = new Log2()
+
                     log2.info("hello info")
                     log2.warn("hello warn")
                     log2.error("hello error")
